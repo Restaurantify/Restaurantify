7cbd10f098226d7acd5f7ef6b109b8a0f8ef0dd7
Stefan <hammerschmiedstefan@gmail.com>
2016-06-09 15:22:59 +0200

    Commit commit

---
+++ app/src/main/java/com/example/stefan/restaurantfiy/RegistrierungActivity.java
@@ -0,0 +1,19 @@
+package com.example.stefan.restaurantfiy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Stefan on 09.06.2016.
+ */
+public class RegistrierungActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrierung);
+        Intent intent = getIntent();
+
+    }
+}

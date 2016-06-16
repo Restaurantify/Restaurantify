11c2197897e4edb133f125ab02a008d9b91a65b3
StefanGschaider <Stefangschaider12@gmail.com>
2016-06-16 13:09:53 +0200

    Bestellung TBL und BestellungHelper
    Und GSON libary in grade file eingefügt!

--- app/src/main/java/com/example/stefan/restaurantfiy/MainActivity.java
+++ app/src/main/java/com/example/stefan/restaurantfiy/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends Activity{
         return true;
     }
 
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will

d53576bfe282864a7a737db12de0f3c763342845
relxied <deixler.felix@gmail.com>
2016-06-16 14:54:39 +0200

    Erstellen des Products Objects Erstellen der Klassen für die Products Tabelle

---
+++ app/src/main/java/com/example/stefan/restaurantfiy/Products.java
@@ -0,0 +1,7 @@
+package com.example.stefan.restaurantfiy;
+
+/**
+ * Created by felixdeixler on 16.06.16.
+ */
+public class Products {
+}
---
+++ app/src/main/java/com/example/stefan/restaurantfiy/ProductsTBL.java
@@ -0,0 +1,9 @@
+package com.example.stefan.restaurantfiy;
+
+/**
+ * Created by felixdeixler on 16.06.16.
+ */
+public class ProductsTBL {
+    public final String productNr = "productNr";
+
+}

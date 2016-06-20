a61b38992ee38dfc15539e486f1ec339bac2570a
relxied <deixler.felix@gmail.com>
2016-06-20 18:53:26 +0200

    Login Klassen löschen

--- app/src/main/java/com/example/stefan/restaurantfiy/Products.java
+++ app/src/main/java/com/example/stefan/restaurantfiy/Products.java
@@ -1,7 +1,29 @@
 package com.example.stefan.restaurantfiy;
 
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 /**
  * Created by felixdeixler on 16.06.16.
  */
-public class Products {
+public class Products extends Activity{
+
+    protected void onCreate()
+    {
+        setContentView(R.layout.products);
+
+        ListView view = (ListView) findViewById(R.id.productsView);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(1,"Test1");
+        list.add(2,"Test2");
+        list.add(3,"Test3");
+
+        ArrayAdapter adapter = new ArrayAdapter();
+        view.setAdapter(adapter);
+    }
+
+
 }

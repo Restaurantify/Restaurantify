0416a27e9b50e1e074e305563bc1610fd34e27bd
relxied <deixler.felix@gmail.com>
2016-06-20 21:58:05 +0200

    Tisch TBL

--- app/src/main/java/com/example/stefan/restaurantfiy/TischTBL.java
+++ app/src/main/java/com/example/stefan/restaurantfiy/TischTBL.java
@@ -6,7 +6,7 @@ package com.example.stefan.restaurantfiy;
 
 public class TischTBL {
 
-    public static final String TABLE_NAME = "Tisch";
+    public static final String TABLE_NAME = "Tische";
 
     public final static String TischId = "TischID";
     public final static String TischNr = "TischNr";
